d638933c9d96ca75035ddba224ae83715fd1a862
TeorgeGakei <spencerparker96@gmail.com>
2018-02-25 11:09:44 -0600

    Missed one uniform
    Whoops.

--- Addon/@CustomUnits/addons/Custom_Units_BLU/Blu_Autorifleman.hpp
+++ Addon/@CustomUnits/addons/Custom_Units_BLU/Blu_Autorifleman.hpp
@@ -3,7 +3,7 @@
 		class Blufor_Autorifleman_1 : B_DUWS_base_F  {
 		scope = 2;
 		displayName = "French Autorifleman"; // In-game name of unit
-		uniformClass = "Niko_USA_R_DCUTan";  // Uniform Class
+		uniformClass = "rhs_uniform_cu_ocp";  // Uniform Class
 		backpack = "VSM_OGA_carryall"; // Backpack Class
 		linkedItems[] = {"lbt_operator_coy", "NeckTight_DMO", "TRYK_r_cap_tan_Glasses", "ItemMap", "ItemCompass", "ItemWatch", "ItemRadio", "ItemGPS"}; // Item's added to the unit. 
 		respawnLinkedItems[] = {"lbt_operator_coy", "NeckTight_DMO", "TRYK_r_cap_tan_Glasses", "ItemMap", "ItemCompass", "ItemWatch", "ItemRadio", "ItemGPS"}; // Item's added to the unit. Should be identical to the linkedItems section.
